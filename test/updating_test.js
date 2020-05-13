910d67cc6157502df7bf4cf42ede0c984c604b1f
Tõnu Säre <tonu.sare@jotautomation.com>
2020-05-13 12:44:26 +0300

    Update Operators

--- test/updating_test.js
+++ test/updating_test.js
@@ -8,7 +8,8 @@ describe('Updating records', function(){
 
     beforeEach(function(done){
             char = new MarioChar({
-            name: 'Mario'
+            name: 'Mario',
+            weight: 50
         });
 
         char.save().then(function(){
@@ -22,6 +23,17 @@ describe('Updating records', function(){
         MarioChar.findOneAndUpdate({name: 'Mario'}, {name: 'Luigi'}).then(function(){
             MarioChar.findOne({_id: char._id}).then(function(result){
                 assert(result.name === 'Luigi');
+                done();
+            });
+        });
+
+    });
+
+    it('Increments the weight by 1', function(done){
+        
+        MarioChar.update({}, { $inc: {weight: 1}}).then(function(){
+            MarioChar.findOne({name: 'Mario'}).then(function(record){
+                assert(record.weight === 51);
                 done();
             })
         });
